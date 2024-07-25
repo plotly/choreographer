b0a6835c6ff973a80a1d83334a9c4728994a7e88
Neyberson <neyberatencio@gmail.com>
2024-07-25 15:02:36 -0500

    Format code

--- devtools/session.py
+++ devtools/session.py
@@ -2,19 +2,19 @@ class Session:
     def __init__(self, command, params):
         self._command = command
         self._params = params
-        
+
     @property
     def command(self):
         return self._command
-    
+
     @command.setter
     def command(self, command):
         self._command = command
-        
+
     @property
     def params(self):
         return self._params
-    
+
     @params.setter
     def params(self, params):
         self._params = params
