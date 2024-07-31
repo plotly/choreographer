5c63b372263c38372be936fb69b9fa70e758cf6d
Neyberson <neyberatencio@gmail.com>
2024-07-31 16:30:31 -0500

    Change the settings about the class attributes

--- devtools/session.py
+++ devtools/session.py
@@ -7,34 +7,10 @@ class Session:
             self._sessionId = sessionId
         else:
             raise TypeError("You must use an string object for sessionId")
-
-        self._eventCbs = {}
-        self._messageCbs = {}
-        self._messageId = 0
-
-        @property
-        def eventCbs(self):
-            return self._eventCbs
-
-        @eventCbs.setter
-        def eventCbs(self, eventCbs):
-            self._eventCbs = eventCbs
-
-        @property
-        def messageCbs(self):
-            return self._messageCbs
-
-        @messageCbs.setter
-        def messageCbs(self, messageCbs):
-            self._messageCbs = messageCbs
-
-        @property
-        def messageId(self):
-            return self._messageId
-
-        @messageId.setter
-        def messageId(self, messageId):
-            self._messageId = messageId
+        
+        self.eventCbs = {}
+        self.messageCbs = {}
+        self.messageId = 0
 
     def send_command(self, command, params, **kwargs):
         cb = kwargs("cb", None)
