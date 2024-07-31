f2afda7eddeccd0456bfbefb3b6b93bfc33aa1be
Neyberson <neyberatencio@gmail.com>
2024-07-31 12:30:09 -0500

    Add attributes sessionId and messageId to Session class

--- devtools/session.py
+++ devtools/session.py
@@ -2,8 +2,13 @@ import json
 
 
 class Session:
-    def __init__(self):
-        pass
+    def __init__(self, sessionId):
+        if isinstance(sessionId, str):
+            self._sessionId = sessionId
+        else:
+            raise TypeError("You must use an string object for sessionId")
+
+        self._messageId = 0
 
     def send_command(self, command, params):
         if not isinstance(command, str):
