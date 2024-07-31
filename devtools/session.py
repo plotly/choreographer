e5046cd19fba7da4208208ac58ab61a0aa5902b4
Neyberson <neyberatencio@gmail.com>
2024-07-31 15:40:16 -0500

    Improve send_command function and improve messageId logic

--- devtools/session.py
+++ devtools/session.py
@@ -9,9 +9,18 @@ class Session:
             raise TypeError("You must use an string object for sessionId")
 
         self._messageId = 0
+        
+        @property
+        def messageId(self):
+            return self._messageId
+        
+        @messageId.setter
+        def messageId(self, messageId):
+            self._messageId = messageId
 
     def send_command(self, command, params):
         if not isinstance(command, str):
             raise TypeError("You must use an string for the command parameter")
-        json_command = {"method": command, "params": params}
+        json_command = {"method": command, "params": params, "menssageId": self.messageId}
+        self.messageId=+1
         return json.dumps(json_command)
