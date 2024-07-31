cd379806a43c4488c0439f7fbabd37c09ae937b7
Neyberson <neyberatencio@gmail.com>
2024-07-31 11:58:45 -0500

    Delete class attributes and add parameters to the send_command function at session.py

--- devtools/session.py
+++ devtools/session.py
@@ -2,28 +2,11 @@ import json
 
 
 class Session:
-    def __init__(self, command, params):
+    def __init__(self):
+        pass
+
+    def send_command(self, command, params):
         if not isinstance(command, str):
             raise TypeError("You must use an string for the command parameter")
-        self._command = command
-        self._params = params
-
-    @property
-    def command(self):
-        return self._command
-
-    @command.setter
-    def command(self, command):
-        self._command = command
-
-    @property
-    def params(self):
-        return self._params
-
-    @params.setter
-    def params(self, params):
-        self._params = params
-
-    def send_command(self):
-        json_command = {"method": self.command, "params": self.params}
+        json_command = {"method": command, "params": params}
         return json.dumps(json_command)
