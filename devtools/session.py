408d888693a033e4c380e5273b629edabf76ccd0
Neyberson <neyberatencio@gmail.com>
2024-07-31 16:12:38 -0500

    Improve send_command with cbs parameter

--- devtools/session.py
+++ devtools/session.py
@@ -11,11 +11,11 @@ class Session:
         self._eventCbs = {}
         self._messageCbs = {}
         self._messageId = 0
-        
+
         @property
         def eventCbs(self):
             return self._eventCbs
-        
+
         @eventCbs.setter
         def eventCbs(self, eventCbs):
             self._eventCbs = eventCbs
@@ -23,7 +23,7 @@ class Session:
         @property
         def messageCbs(self):
             return self._messageCbs
-        
+
         @messageCbs.setter
         def messageCbs(self, messageCbs):
             self._messageCbs = messageCbs
@@ -31,14 +31,24 @@ class Session:
         @property
         def messageId(self):
             return self._messageId
-        
+
         @messageId.setter
         def messageId(self, messageId):
             self._messageId = messageId
 
-    def send_command(self, command, params):
+    def send_command(self, command, params, **kwargs):
+        cb = kwargs("cb", None)
+
+        if cb:
+            self.messageCbs[self.messageId] = cb
+
         if not isinstance(command, str):
             raise TypeError("You must use an string for the command parameter")
-        json_command = {"method": command, "params": params, "menssageId": self.messageId}
-        self.messageId=+1
+        json_command = {
+            "method": command,
+            "params": params,
+            "menssageId": self.messageId,
+            "messageCbs": self.messageCbs,
+        }
+        self.messageId = +1
         return json.dumps(json_command)
