c86e955b5b5180173ab8f01873502a478b8ee8ba
Neyberson <neyberatencio@gmail.com>
2024-08-01 15:42:58 -0500

    Change *args to browser at Connection class

--- devtools/connection.py
+++ devtools/connection.py
@@ -3,12 +3,12 @@ from collections import OrderedDict
 
 
 class Connection:
-    def __init__(self, *args):
-        self.session = Session(*args)
+    def __init__(self, browser):
+        self.session = Session(browser)
         self.sessions = OrderedDict()
         self.sessions[self.session.sessionId] = self.session
 
-    def add_session(self, *args):
-        user_session = Session(*args)
+    def add_session(self, browser):
+        user_session = Session(browser)
         self.session = user_session
         self.sessions[self.session.sessionId] = self.session
