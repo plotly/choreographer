46cf4186b186ff76363f785130a30633acf84e4f
Neyberson <neyberatencio@gmail.com>
2024-08-01 12:42:20 -0500

    Create Connection class

---
+++ devtools/connection.py
@@ -0,0 +1,8 @@
+from session import Session
+from collections import OrderedDict
+
+class Connection:
+    def __init__(self, *args):
+        self.session = Session(*args)
+        od = OrderedDict()
+        self.sessions = od[self.session.sessionId] = self.sessiongit
