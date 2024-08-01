77eb270bf8a06a08a1cabeeb2517e8cfb7afabb8
Neyberson <neyberatencio@gmail.com>
2024-08-01 16:25:42 -0500

    Change value at Session instance from Connection class

--- devtools/connection.py
+++ devtools/connection.py
@@ -4,6 +4,6 @@ from collections import OrderedDict
 
 class Connection:
     def __init__(self, browser):
-        self.session = Session(browser)
+        self.session = Session(sessionId="")
         self.sessions = OrderedDict()
         self.sessions[self.session.sessionId] = self.session
