890c2b8bb40c6bf91237303a54f588ce779ea6ef
Neyberson <neyberatencio@gmail.com>
2024-08-02 16:22:27 -0500

    Create create_tab, open_tab, add_tab and close_tab functions at Coonection class

--- devtools/connection.py
+++ devtools/connection.py
@@ -7,3 +7,23 @@ class Connection:
         self.browser_process = browser_process
         self.browser_session = Session(sessionId="")
         self.tab_sessions = OrderedDict()
+
+    i=0
+    def create_tab(self):
+        global i
+        self.browser_session = Session(str(i))
+        global i = i + 1
+        self.tab_sessions[self.browser_session.sessionId] = self.browser_session
+        print("The session was created and added!")
+
+    def open_tab(self):
+        print(self.tab_sessions)
+
+    def add_tab(self, session_id):
+        self.browser_session = Session(session_id)
+        self.tab_sessions[self.browser_session.sessionId] = self.browser_session
+        print("The session was added!")
+    
+    def close_tab(self):
+        self.tab_sessions = OrderedDict()
+        print("The sessions was deleted!")
