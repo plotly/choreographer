f9ef6c5e87eb84ef4b1df92fdab0326f2ce8c465
Neyberson <neyberatencio@gmail.com>
2024-08-05 10:36:42 -0500

    Delete list_session_id attribute and improve the logic at create_tab and open_tab methods at connection.py

--- devtools/connection.py
+++ devtools/connection.py
@@ -8,19 +8,17 @@ class Connection:
         self.browser_process = browser_process
         self.browser_session = Session(session_id="")
         self.tab_sessions = OrderedDict()
-        self._list_session_id = []
 
     def create_tab(self):
         str_uuid = str(uuid.uuid4())
-        self._list_session_id = self._list_session_id.append(str_uuid)
         self.browser_session = Session(str_uuid)
         self.tab_sessions[self.browser_session.session_id] = self.browser_session
         print("The session were created and added!")
 
     def open_tab(self):
         print("Current sessions:")
-        for session_id, session_instance in self._list_session_id, self.tab_sessions:
-            print(f"Session ID: {session_id}, Session instance: {session_instance}")
+        for session_id in self.tab_sessions:
+            print(f"Session ID: {session_id}, Session instance: {self.tab_sessions[session_id]}")
 
     def add_tab(self, session_id):
         self.browser_session = Session(session_id)
