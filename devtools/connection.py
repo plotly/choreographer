91c84fe5699c5e56d273a63af11256617da195e7
Neyberson <neyberatencio@gmail.com>
2024-08-05 09:31:56 -0500

    Improve strategy at create_tab and open_tab with list_sessionId

--- devtools/connection.py
+++ devtools/connection.py
@@ -8,16 +8,19 @@ class Connection:
         self.browser_process = browser_process
         self.browser_session = Session(sessionId="")
         self.tab_sessions = OrderedDict()
+        self._list_sessionId = []
 
     def create_tab(self):
-        self.browser_session = Session(str(uuid.uuid4()))
+        str_uuid = str(uuid.uuid4())
+        self._list_sessionId = self._list_sessionId.append(str_uuid)
+        self.browser_session = Session(str_uuid)
         self.tab_sessions[self.browser_session.sessionId] = self.browser_session
         print("The session were created and added!")
 
     def open_tab(self):
         print("Current sessions:")
-        for session_id in self.tab_sessions:
-            print(f"Session ID: {session_id}")
+        for session_id, session_instance in self._list_sessionId, self.tab_sessions:
+            print(f"Session ID: {session_id}, Session instance: {session_instance}")
 
     def add_tab(self, session_id):
         self.browser_session = Session(session_id)
