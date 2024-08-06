edaa0089b6887b797433a68608e4408cc3be89e1
Andrew Pikul <ajpikul@gmail.com>
2024-08-06 17:57:08 -0400

    Improve comments

--- app/app.py
+++ app/app.py
@@ -4,17 +4,30 @@ from app_utils import start_browser
 
 def main():
     # Interface Test
+
+    ## Create Connection
     browser = devtools.Connection()
+
     browser.list_tabs()
+
+    ## Simulate Commands
     print(browser.browser_session.send_command("command1", dict(param1="1", param2="a")))
     print(browser.browser_session.send_command("command2", dict(param1="2", param2="b")))
+
+    ## Create Tab
     myTab = browser.create_tab()
+    browser.list_tabs()
+
+    ## Simulate Commands
     print(myTab.send_command("tabCommand1", dict(param1="tab1", param2="taba")))
     print(myTab.send_command("tabCommand2", dict(param1="tab2", param2="tabb")))
-    browser.list_tabs()
+    print(browser.browser_session.send_command("command3", dict(param1="3", param2="c")))
+
+    ## Close Tab
     browser.close_tab(myTab)
     browser.list_tabs()
-    print(browser.browser_session.send_command("command3", dict(param1="3", param2="c")))
+
+    ## Simulate Commands
     print(browser.browser_session.send_command("command4", dict(param1="4", param2="d")))
 
     # Process/Pipes Test
