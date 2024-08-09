1d67e2bddf06e40b4f8810829d1ea3ce00d24686
Neyberson <neyberatencio@gmail.com>
2024-08-09 16:54:31 -0500

    Format code app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import devtools
 import time
 
+
 def main():
     # Interface Test
 
@@ -10,8 +11,16 @@ def main():
     connection.list_tabs()
 
     ## Simulate Commands
-    print(connection.browser_session.send_command("command1", dict(param1="1", param2="a")))
-    print(connection.browser_session.send_command("command2", dict(param1="2", param2="b")))
+    print(
+        connection.browser_session.send_command(
+            "command1", dict(param1="1", param2="a")
+        )
+    )
+    print(
+        connection.browser_session.send_command(
+            "command2", dict(param1="2", param2="b")
+        )
+    )
 
     ## Create Tab
     myTab = connection.create_tab()
@@ -20,14 +29,22 @@ def main():
     ## Simulate Commands
     print(myTab.send_command("tabCommand1", dict(param1="tab1", param2="taba")))
     print(myTab.send_command("tabCommand2", dict(param1="tab2", param2="tabb")))
-    print(connection.browser_session.send_command("command3", dict(param1="3", param2="c")))
+    print(
+        connection.browser_session.send_command(
+            "command3", dict(param1="3", param2="c")
+        )
+    )
 
     ## Close Tab
     connection.close_tab(myTab)
     connection.list_tabs()
 
     ## Simulate Commands
-    print(connection.browser_session.send_command("command4", dict(param1="4", param2="d")))
+    print(
+        connection.browser_session.send_command(
+            "command4", dict(param1="4", param2="d")
+        )
+    )
 
     # Process/Pipes Test
     browser = devtools.Browser()
@@ -35,13 +52,14 @@ def main():
 
     pipe.write("{}")
     print(pipe.read_jsons(debug=True))
-    print(pipe.read_jsons(blocking=False,debug=True))
-    print(pipe.read_jsons(blocking=False,debug=True))
-    print(pipe.read_jsons(blocking=False,debug=True))
+    print(pipe.read_jsons(blocking=False, debug=True))
+    print(pipe.read_jsons(blocking=False, debug=True))
+    print(pipe.read_jsons(blocking=False, debug=True))
 
     time.sleep(10)
 
     browser.close_browser()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
