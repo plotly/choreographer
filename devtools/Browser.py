8ff1a7542f46701414ceceefa9a000aac00e2c36
Neyberson <neyberatencio@gmail.com>
2024-08-09 09:37:26 -0500

    Create start_browser() at Browser class

--- devtools/Browser.py
+++ devtools/Browser.py
@@ -1,7 +1,36 @@
 from app.app_utils import Pipe
 from collections import OrderedDict
+import platform
+import os
+import sys
+import subprocess
 
 class Browser:
     def __init__(self):
         self.pipes = OrderedDict()
         self.subprocess = OrderedDict()
+
+    def start_browser(self, path=None):
+        pipe = Pipe()
+        if not path:
+            if platform.system() == "Windows":
+                path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+            elif platform.system() == "Linux":
+                path = "/usr/bin/google-chrome-stable"
+            else:
+                raise ValueError("You must set path to a chrome-like browser")
+        new_env = os.environ.copy()
+        new_env['CHROMIUM_PATH']=path
+        win_only = {}
+        if platform.system() == "Windows":
+            win_only = {"creationflags":subprocess.CREATE_NEW_PROCESS_GROUP}
+        proc = subprocess.Popen(
+                [sys.executable, os.path.join(os.path.dirname(os.path.realpath(__file__)), "chrome_wrapper.py")],
+                close_fds=True,
+                stdin=pipe.read_to_chromium,
+                stdout=pipe.write_from_chromium,
+                stderr=None,
+                env=new_env,
+                **win_only
+                )
+        return (proc, pipe)
