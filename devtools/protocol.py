6d4b6f4f441fd3c12da7830ec4c8baa525a7a909
Neyberson <neyberatencio@gmail.com>
2024-08-13 12:01:50 -0500

    Create list_tab_id list and logic to use id from Tab at Protocol

--- devtools/protocol.py
+++ devtools/protocol.py
@@ -6,12 +6,15 @@ from collections import OrderedDict
 class Protocol:
     def __init__(self, browser_pipe=Pipe()):
         self.browser_tab = Tab()
+        self.list_tab_id = [self.browser_tab.id]
         self.tabs = OrderedDict()
         self.browser_pipe = browser_pipe
 
     def create_tab(self):
         tab_obj = Tab()
-        self.tabs[id(tab_obj)] = tab_obj
+        self.list_tab_id = self.list_tab_id.append(self.list_tab_id[-1] + 1)
+        tab_obj.id = self.list_tab_id[-1]
+        self.tabs[tab_obj] = tab_obj
         print("New Tab Created")
         return tab_obj
 
