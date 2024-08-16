b7d6496b0d95fa8794dd66aa8e55826a7d0b4856
Neyberson <neyberatencio@gmail.com>
2024-08-16 17:16:46 -0500

    Simplify code at app.py

--- app/app.py
+++ app/app.py
@@ -6,18 +6,16 @@ def main():
     # Interface Test
     # Process/Pipes Test
     with devtools.Browser(headless=False) as browser:
-        ## Create Protocol
-        connection = browser.protocol
 
-        connection.create_tab()
-        connection.create_tab()
-        connection.create_tab()
+        browser.create_tab()
+        browser.create_tab()
+        browser.create_tab()
         time.sleep(10)
 
-        connection.pipe.read_jsons(debug=True)
-        connection.pipe.read_jsons(blocking=False, debug=True)
-        connection.pipe.read_jsons(blocking=False, debug=True)
-        connection.pipe.read_jsons(blocking=False, debug=True)
+        browser.pipe.read_jsons(debug=True)
+        browser.pipe.read_jsons(blocking=False, debug=True)
+        browser.pipe.read_jsons(blocking=False, debug=True)
+        browser.pipe.read_jsons(blocking=False, debug=True)
 
         time.sleep(10)
 
