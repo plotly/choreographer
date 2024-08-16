42bc7da7f1fd7f1f841d18229e2417986b9a2d75
Neyberson <neyberatencio@gmail.com>
2024-08-16 17:17:14 -0500

    Format code at app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,6 @@ def main():
     # Interface Test
     # Process/Pipes Test
     with devtools.Browser(headless=False) as browser:
-
         browser.create_tab()
         browser.create_tab()
         browser.create_tab()
