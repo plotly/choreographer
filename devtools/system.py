c9135fe2427b46bf6d5f48d608918e92c91f565c
Neyberson <neyberatencio@gmail.com>
2024-08-20 16:30:57 -0500

    Format code at system.py

--- devtools/system.py
+++ devtools/system.py
@@ -9,14 +9,24 @@ chromium = ["chromium"]
 # edge = // this needs to be tested
 
 
-def which_windows(): 
+def which_windows():
     import winreg
     import re
-    command = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, "ChromeHTML\\shell\\open\\command", 0, winreg.KEY_READ), "")[0]
-    exe=re.search("\"(.*?)\"", command).group(1)
+
+    command = winreg.QueryValueEx(
+        winreg.OpenKey(
+            winreg.HKEY_CLASSES_ROOT,
+            "ChromeHTML\\shell\\open\\command",
+            0,
+            winreg.KEY_READ,
+        ),
+        "",
+    )[0]
+    exe = re.search('"(.*?)"', command).group(1)
     return exe
 
-def which_browser(executable_name = chrome):
+
+def which_browser(executable_name=chrome):
     path = None
     if isinstance(executable_name, str):
         executable_name = [executable_name]
@@ -26,10 +36,13 @@ def which_browser(executable_name = chrome):
                 path = which_windows()
                 print(path)
                 break
-            except: # noqa # no bare except according to ruff but who knows what errors we'll get from this
-                os.environ['NoDefaultCurrentDirectoryInExePath']="0"
+            except:  # noqa # no bare except according to ruff but who knows what errors we'll get from this
+                os.environ["NoDefaultCurrentDirectoryInExePath"] = "0"
         path = shutil.which(exe)
-        if path: break
+        if path:
+            break
     if not path:
-        raise RuntimeError("Could not find chrome. Please path execution path as environmental variable BROWSER_PATH")
+        raise RuntimeError(
+            "Could not find chrome. Please path execution path as environmental variable BROWSER_PATH"
+        )
     return path
