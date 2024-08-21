aced63ccbd1fb4d4165f7263bcf27941f74ed770
Neyberson <neyberatencio@gmail.com>
2024-08-21 15:23:11 -0500

    Use verify_json() into create_tab() from Protocol

--- devtools/protocol.py
+++ devtools/protocol.py
@@ -38,6 +38,7 @@ class Protocol:
             command="Target.createTarget", params={"url": "chrome://new-tab-page/"}
         )
         data = self.pipe.read_jsons(debug=True)
+        self.verify_json(data)
         json_obj = self.verify_json_id(data)
         tab_obj.target_id = json_obj["result"]["targetId"]
         self.tabs[tab_obj.target_id] = tab_obj
