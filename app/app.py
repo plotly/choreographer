4fedaded69a4dda1f7f64ccefcf1af1934466035
Neyberson <neyberatencio@gmail.com>
2024-08-22 09:55:52 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,6 +14,19 @@ def main():
         browser.create_tab()
         browser.create_tab()
         browser.create_tab()
+        print(browser.protocol.tabs[next(reversed(browser.protocol.tabs))])
+        browser.protocol.tabs[next(reversed(browser.protocol.tabs))].add_session()
+        tab = browser.protocol.tabs[next(reversed(browser.protocol.tabs))]
+        browser.protocol.send_command(
+            command="Page.navigate",
+            params={"url": "https://github.com/"},
+            session_id=next(reversed(tab.tab_sessions)),
+        )
+        time.sleep(4)
+        browser.protocol.send_command(
+            command="Target.createTarget", params={"url": "https://www.youtube.com"}
+        )
+        time.sleep(5)
         browser.protocol.send_command(command="Target.getTargets")
         browser.protocol.pipe.read_jsons(debug=True)
         browser.protocol.pipe.read_jsons(blocking=False, debug=True)
@@ -27,6 +40,7 @@ def main():
         browser.protocol.pipe.read_jsons(blocking=False, debug=True)
         time.sleep(3)
     time.sleep(3)
+
 
 if __name__ == "__main__":
     main()
