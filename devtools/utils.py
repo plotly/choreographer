5b67a9a2a01b1c190c88aa71287700958850a16a
Neyberson <neyberatencio@gmail.com>
2024-08-23 10:14:43 -0500

    Add session_id parameter at verify_json_id() from utils.py

--- devtools/utils.py
+++ devtools/utils.py
@@ -2,10 +2,11 @@ import os
 import json
 
 
-def verify_json_id(json_list, message_id):
+def verify_json_id(json_list, message_id, session_id=None):
     for json_ in json_list:
         if message_id in json_ and message_id == json["id"]:
-            return json_
+            if session_id in json_ and session_id == json["sessionId"]:
+                return json_
 
 
 def verify_json_error(json):
