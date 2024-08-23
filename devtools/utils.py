5656ece88075c739f4406b0ff615937ee8518530
Neyberson <neyberatencio@gmail.com>
2024-08-23 10:30:38 -0500

    Add target_id parameter at verify_json_id() from utils.py

--- devtools/utils.py
+++ devtools/utils.py
@@ -2,12 +2,17 @@ import os
 import json
 
 
-def verify_json_id(json_list, message_id, session_id=None):
+def verify_json_id(json_list, message_id, session_id=None, target_id=None):
     for json_ in json_list:
         if message_id in json_ and message_id == json["id"]:
             if session_id in json_ and session_id == json["sessionId"]:
                 return json_
-
+            elif target_id in json_:
+                if (
+                    "results" in json_ and target_id == json_["results"]["target_id"]
+                ) or (target_id == json_["target_id"]):
+                    return json_
+    return None
 
 def verify_json_error(json):
     if "error" in json:
