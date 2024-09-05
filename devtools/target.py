f92ce2a7b2b4d3d65a0c785342b69b71e6ae5072
Neyberson <neyberatencio@gmail.com>
2024-09-05 15:54:56 -0500

    Create close_session() in Target

--- devtools/target.py
+++ devtools/target.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 from .session import Session
 
+
 class Target:
     def __init__(self, target_id, browser):
         if not isinstance(target_id, str):
@@ -23,24 +24,33 @@ class Target:
 
     async def create_session(self):
         if not self.browser.loop:
-            raise RuntimeError("There is no eventloop, or was not passed to browser. Cannot use async methods")
-        response = await self.browser.send_command("Target.attachToTarget",
-                                                  params=dict(
-                                                      targetId = self.target_id,
-                                                      flatting=True
-                                                      ))
+            raise RuntimeError(
+                "There is no eventloop, or was not passed to browser. Cannot use async methods"
+            )
+        response = await self.browser.send_command(
+            "Target.attachToTarget", params=dict(targetId=self.target_id, flatting=True)
+        )
         if "error" in response:
-            raise RuntimeError("Could not create session") from Exception(response["error"])
+            raise RuntimeError("Could not create session") from Exception(
+                response["error"]
+            )
         session_id = response["result"]["sessionId"]
         new_session = Session(self, session_id)
         self.add_session(new_session)
         return new_session
 
-    # def close_session():
+    def close_session(self, session):
+        if isinstance(session, Session):
+            session_id = session.session_id
+        data_command = self.send_command(
+            command="Target.detachFromTarget",
+            params={"sessionId": session_id},
+        )
+        print(f"The session {session_id} has been closed")
+        self.remove_session(session)
+        return data_command
 
     def send_command(self, command, params=None):
         if not self.sessions.values():
             raise RuntimeError("Cannot send_command without at least one valid session")
         return list(self.sessions.values())[0].send_command(command, params)
-
-
