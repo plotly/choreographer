8148cc44fe77ffb18225b8f630c4594bf686621c
Andrew Pikul <ajpikul@gmail.com>
2024-09-09 13:50:35 -0400

    Don't delete non-existent keys

--- devtools/target.py
+++ devtools/target.py
@@ -23,6 +23,7 @@ class Target:
     def remove_session(self, session_id):
         if isinstance(session_id, Session):
             session_id = session_id.session_id
+        if session_id not in self.sessions: return
         del self.sessions[session_id]
 
     async def create_session(self):
