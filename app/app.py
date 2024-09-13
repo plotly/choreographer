23021550c6f4f917bf855a70b4b2be568ca25a30
Andrew Pikul <ajpikul@gmail.com>
2024-09-13 12:54:11 -0400

    Simplify app.py

--- app/app.py
+++ app/app.py
@@ -6,33 +6,17 @@ def main_sync():
     # Interface Test
     # Process/Pipes Test
     with devtools.Browser(headless=False, debug=True, debug_browser=True) as browser:
+        browser.protocol.run_output_thread()
+        time.sleep(2)
         browser.send_command(command="Target.getTargets")
-        browser.protocol.pipe.read_jsons()
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
-        time.sleep(4)
+        time.sleep(3)
         browser.send_command(
             command="Target.createTarget", params={"url": "https://www.youtube.com"}
         )
-        browser.protocol.pipe.read_jsons()
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
-        time.sleep(5)
-        browser.send_command(command="Target.getTargets")
-        browser.protocol.pipe.read_jsons()
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
         time.sleep(3)
         browser.send_command(command="Target.getTargets")
-        browser.protocol.pipe.read_jsons()
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
-        browser.protocol.pipe.read_jsons(blocking=False)
         time.sleep(3)
-    time.sleep(3)
+        print("Done")
 
 async def main_async():
     with devtools.Browser(headless=False, debug=True, loop=asyncio.get_running_loop(), debug_browser=True) as browser:
@@ -46,13 +30,13 @@ async def main_async():
         await browser_session2.send_command("Target.getTargets")
         await tab2.close_session(tab2_session2)
         await browser.close_tab(tab2)
-        print("All is done")
+        print("All is done") # do we need to close loop?
         await asyncio.sleep(5)
 
 if __name__ == "__main__":
     main_sync()
-    time.sleep(2)
-    asyncio.run(main_async())
+    #time.sleep(2)
+    #asyncio.run(main_async())
 
 
 # blocking, regular blocking, you read and write, good luck (we need to be able to piece-meal the thing together)
