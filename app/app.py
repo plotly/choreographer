780ec971132611336ba117cd62552ffb7795d955
Andrew Pikul <ajpikul@gmail.com>
2024-09-13 13:40:28 -0400

    Change trivially app.py

--- app/app.py
+++ app/app.py
@@ -30,13 +30,13 @@ async def main_async():
         await browser_session2.send_command("Target.getTargets")
         await tab2.close_session(tab2_session2)
         await browser.close_tab(tab2)
-        print("All is done") # do we need to close loop?
-        await asyncio.sleep(5)
+    print("All is done") # do we need to close loop?
 
 if __name__ == "__main__":
-    main_sync()
+    #main_sync()
     #time.sleep(2)
-    #asyncio.run(main_async())
+    asyncio.run(main_async())
+    time.sleep(1)
 
 
 # blocking, regular blocking, you read and write, good luck (we need to be able to piece-meal the thing together)
