0a4d0070681dd0190f5f980deb01be0e1d85a22e
Neyberson <neyberatencio@gmail.com>
2024-09-24 09:34:28 -0500

    Create test_tab.py

---
+++ tests/test_tab.py
@@ -0,0 +1,76 @@
+import pytest
+import devtools
+import asyncio
+
+
+url = (
+    "https://plotly.com/",
+    "https://plotly.com/python/",
+    "https://plotly.com/graphing-libraries/",
+    "https://plotly.com/python/getting-started/",
+)
+
+
+async def print_obj(obj):
+    print(obj)
+
+
+@pytest.mark.parametrize(
+    "test_input_headless,test_input_debug,test_input_debug_browser",
+    [
+        (True, True, True),
+        (True, True, False),
+        (True, False, True),
+        (True, False, False),
+        (False, True, True),
+        (False, True, False),
+        (False, False, True),
+        (False, False, False),
+    ],
+    ids=[
+        "async_tab_1",
+        "async_tab_2",
+        "async_tab_3",
+        "async_tab_4",
+        "async_tab_5",
+        "async_tab_6",
+        "async_tab_7",
+        "async_tab_8",
+    ],
+)
+@pytest.mark.asyncio
+async def test_async_browser(
+    test_input_headless, test_input_debug, test_input_debug_browser
+):
+    async with devtools.Browser(
+        headless=test_input_headless,
+        debug=test_input_debug,
+        debug_browser=test_input_debug_browser,
+    ) as browser:
+        tab_1 = await browser.create_tab(url[0])
+        tab_2 = await browser.create_tab(url[1])
+        session_1 = await tab_1.create_session()
+        session_2 = await tab_1.create_session()
+        assert isinstance(tab_1, devtools.tab.Tab)
+        assert isinstance(tab_2, devtools.tab.Tab)
+        assert isinstance(session_1, devtools.session.Session)
+        assert isinstance(session_2, devtools.session.Session)
+        assert await tab_1.close_session(session_1) is not None
+        tab_1.remove_session(session_2)
+        assert session_2.session_id not in tab_1.sessions
+        assert tab_2.add_session(session_2) is None
+        assert session_2.session_id in tab_2.sessions
+        assert tab_1.send_command("Page.enable")
+        assert tab_1.subscribe_once("Page") is None
+        assert "Page" in list(tab_1.sessions.values())[0].subscriptions_futures
+        assert tab_1.subscribe("*", print_obj, True) is None
+        assert tab_1.subscribe("INVALID", print_obj, False) is None
+        assert "*" in list(tab_1.sessions.values())[0].subscriptions
+        assert tab_1.unsubscribe("INVALID") is None
+        assert "INVALID" not in list(tab_1.sessions.values())[0].subscriptions
+        await tab_1.send_command(
+            "Page.navigate", params=dict(url="https://youtube.com")
+        ) is not None
+        await tab_2.send_command(
+            "Page.navigate", params=dict(url="https://plotly.com/")
+        ) is not None
