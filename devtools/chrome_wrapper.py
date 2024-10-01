100bec901c4965b1a2aa238272596ecfd5620329
Andrew Pikul <ajpikul@gmail.com>
2024-10-01 13:48:36 -0400

    Readd close fds to see test

--- devtools/chrome_wrapper.py
+++ devtools/chrome_wrapper.py
@@ -56,7 +56,7 @@ def open_browser(to_chromium, from_chromium, stderr=None, env=None, loop=None, l
             f"--remote-debugging-io-pipes={str(to_chromium_handle)},{str(from_chromium_handle)}"
         ]
         if platform.system() == "Windows":
-            win_only = {"creationflags": subprocess.CREATE_NEW_PROCESS_GROUP}
+            win_only = {"creationflags": subprocess.CREATE_NEW_PROCESS_GROUP, "close_fds":False}
     if not loop:
         return subprocess.Popen(
             cli,
