cb1673de03742279f3c0d6064e41b3ca0e005c3f
Neyberson <neyberatencio@gmail.com>
2024-10-03 15:13:06 -0500

    Change strategy for Browser open and close in test_process.py

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,14 +1,14 @@
 import pytest
 import devtools
 
+
 @pytest.mark.asyncio
-async def test_async_tab(
-    headless, debug, debug_browser
-):
-    async with devtools.Browser(
+async def test_async_tab(headless, debug, debug_browser):
+    browser = await devtools.Browser(
         headless=headless,
         debug=debug,
         debug_browser=debug_browser,
-    ) as browser:
-        response = await browser.send_command(command="Target.getTargets")
-        assert "result" in response and "targetInfos" in response["result"]
+    )
+    response = await browser.send_command(command="Target.getTargets")
+    assert "result" in response and "targetInfos" in response["result"]
+    await browser.close()
