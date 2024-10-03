f768f5627798ae37c7ca5615e5998e62ffc5d971
Neyberson <neyberatencio@gmail.com>
2024-10-03 16:42:28 -0500

    Change ids in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,18 @@
 import pytest
 
 
-@pytest.fixture(params=[True, False], ids=["async_headless", "async_no_headless"])
+@pytest.fixture(params=[True, False], ids=["headless", "no_headless"])
 def headless(request):
     return request.param
 
 
-@pytest.fixture(params=[True, False], ids=["async_debug", "async_no_debug"])
+@pytest.fixture(params=[True, False], ids=["debug", "no_debug"])
 def debug(request):
     return request.param
 
 
 @pytest.fixture(
-    params=[True, False], ids=["async_debug_browser", "async_no_debug_browser"]
+    params=[True, False], ids=["debug_browser", "no_debug_browser"]
 )
 def debug_browser(request):
     return request.param
