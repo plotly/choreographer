8875d3df2fe2248990e4e87e4a774eb5af372451
Neyberson <neyberatencio@gmail.com>
2024-10-03 15:23:32 -0500

    Improve test_process.py to test with context and no context

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,9 +1,20 @@
 import pytest
 import devtools
 
+@pytest.mark.asyncio
+async def test_async_with_context_process(
+    headless, debug, debug_browser
+):
+    async with devtools.Browser(
+        headless=headless,
+        debug=debug,
+        debug_browser=debug_browser,
+    ) as browser:
+        response = await browser.send_command(command="Target.getTargets")
+        assert "result" in response and "targetInfos" in response["result"]
 
 @pytest.mark.asyncio
-async def test_async_tab(headless, debug, debug_browser):
+async def test_async_no_context_process(headless, debug, debug_browser):
     browser = await devtools.Browser(
         headless=headless,
         debug=debug,
