586fbaffc22d0b2b42bf133b1255223db7ec3952
Neyberson <neyberatencio@gmail.com>
2024-10-04 17:37:26 -0500

    Change scope to session in some fixtures in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,12 +23,12 @@ def pytest_addoption(parser):
     parser.addoption("--debug-tests", action="store_true", default=False)
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def intern_headless(request):
     return request.config.getoption("--headless")
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def intern_debug(request):
     return request.config.getoption("--debug-tests")
 
