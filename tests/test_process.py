c42bf2113e6db8d1186768280e51041b17ab9ea3
Andrew Pikul <ajpikul@gmail.com>
2024-10-04 00:37:16 -0400

    Use try/finally to make sure browser closes

--- tests/test_process.py
+++ tests/test_process.py
@@ -27,20 +27,9 @@ async def test_no_context(headless, debug, debug_browser):
         debug=debug,
         debug_browser=debug_browser,
     )
-
-    # errors in this test will cause browser not to close
-    # so lets make sure it does
-    loop = asyncio.get_running_loop()
-    exception_handler = loop.get_exception_handler()
-    def close_browser(loop, context):
-        browser.close() # posts task, doesn't need to be awaited
-        if exception_handler:
-            exception_handler(loop, context)
-        else:
-            loop.default_exception_handler(context)
-    loop.set_exception_handler(close_browser)
-
-    response = await browser.send_command(command="Target.getTargets")
-    assert "result" in response and "targetInfos" in response["result"]
-    assert len(response["result"]["targetInfos"]) != 0
-    await browser.close()
+    try:
+        response = await browser.send_command(command="Target.getTargets")
+        assert "result" in response and "targetInfos" in response["result"]
+        assert len(response["result"]["targetInfos"]) != 0
+    finally:
+        await browser.close()
