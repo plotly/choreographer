0ee933025ff02036ab284813b4a74112ff8115f6
Andrew Pikul <ajpikul@gmail.com>
2024-10-03 22:38:09 -0400

    Shorten variable names

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,16 @@
 import pytest
 
 
-@pytest.fixture(params=[True, False], ids=["headless", "no_headless"])
+@pytest.fixture(params=[True, False], ids=["headless", ""])
 def headless(request):
     return request.param
 
 
-@pytest.fixture(params=[True, False], ids=["debug", "no_debug"])
+@pytest.fixture(params=[True, False], ids=["debug", ""])
 def debug(request):
     return request.param
 
 
-@pytest.fixture(
-    params=[True, False], ids=["debug_browser", "no_debug_browser"]
-)
+@pytest.fixture(params=[True, False], ids=["debug_browser", ""])
 def debug_browser(request):
     return request.param
