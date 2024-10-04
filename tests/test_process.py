d70574a6f4451134421c1633d686aea67b995b1c
Andrew Pikul <ajpikul@gmail.com>
2024-10-03 23:50:15 -0400

    Check CI continues if only using context

--- tests/test_process.py
+++ tests/test_process.py
@@ -18,16 +18,16 @@ async def test_context(
     ) as browser:
         response = await browser.send_command(command="Target.getTargets")
         assert "result" in response and "targetInfos" in response["result"]
-        assert len(response["result"]["targetInfos"]) != 0
+        assert (len(response["result"]["targetInfos"]) != 0)
 
-@pytest.mark.asyncio
-async def test_no_context(headless, debug, debug_browser):
-    browser = await devtools.Browser(
-        headless=headless,
-        debug=debug,
-        debug_browser=debug_browser,
-    )
-    response = await browser.send_command(command="Target.getTargets")
-    assert "result" in response and "targetInfos" in response["result"]
-    assert len(response["result"]["targetInfos"]) != 0
-    await browser.close()
+#@pytest.mark.asyncio
+#async def test_no_context(headless, debug, debug_browser):
+#    browser = await devtools.Browser(
+#        headless=headless,
+#        debug=debug,
+#        debug_browser=debug_browser,
+#    )
+#    response = await browser.send_command(command="Target.getTargets")
+#    assert "result" in response and "targetInfos" in response["result"]
+#    assert len(response["result"]["targetInfos"]) != 0
+#    await browser.close()
