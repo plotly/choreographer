0f19075881e448eab0298c12bf2807f9635bcd6e
Neyberson <neyberatencio@gmail.com>
2024-10-07 11:40:15 -0500

    Fix bug about session_id and improve some tests in test_browser.py

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -21,18 +21,9 @@ async def test_create_and_close_tab(browser):
 async def test_create_and_close_session(browser):
     session = await browser.create_session()
     assert isinstance(session, devtools.session.Session)
-    assert session.target_id in browser.sessions
+    assert session.session_id in browser.sessions
     await browser.close_session(session)
     assert session.session_id not in browser.sessions
-
-
-@pytest.mark.asyncio
-async def test_populate_targets(browser):
-    await browser.populate_targets()
-    if browser.headless is False:
-        assert len(browser.tabs) > 1
-    else:
-        assert len(browser.tabs) == 1
 
 
 @pytest.mark.asyncio
@@ -47,15 +38,28 @@ async def test_browser_send_command(browser):
     assert "result" in response and "targetInfos" in response["result"]
 
 
+@pytest.mark.asyncio
+async def test_populate_targets(browser):
+    await browser.send_command(command="Target.createTarget", params={"url": url[1]})
+    await browser.populate_targets()
+    if browser.headless is False:
+        assert len(browser.tabs) > 1
+    else:
+        assert len(browser.tabs) == 1
+
+
 def test_get_tab(browser):
-    assert browser.get_tab() == list(browser.tabs.values())[0]
+    if browser.headless:
+        assert browser.get_tab() is None
+    else:
+        assert browser.get_tab() == list(browser.tabs.values())[0]
 
 
 def test_sync_browser(headless, debug, debug_browser):
-    with devtools.Browser(
+    browser = devtools.Browser(
         headless=headless,
         debug=debug,
         debug_browser=debug_browser,
-    ) as browser:
-        response = browser.send_command(command="Target.createTarget", params={"url": url[1]})
-        assert "result" in response
+    )
+    assert isinstance(browser, devtools.browser.Browser)
+    browser.close()
