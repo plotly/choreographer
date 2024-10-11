ec8f080da10d9d2ef3fa8b2282f15a1f08d8537d
Andrew Pikul <ajpikul@gmail.com>
2024-10-11 10:46:59 -0400

    Further format output

--- app/test1.py
+++ app/test1.py
@@ -11,20 +11,20 @@ browser.close()
 
 time.sleep(1)
 
-print("Counting:")
+print("Counting".center(20, "-"))
 count = browser._retry_delete_manual(old_temp_file)
 print(f"Found {count[0]} files, {count[1]} directories")
 print(f"Errors: {count[2]}")
 
 time.sleep(1)
 
-print("Deleting:")
+print("Deleting".center(20, "-"))
 count = browser._retry_delete_manual(old_temp_file, delete=True)
 print(f"Errors: {count[2]}")
 
 time.sleep(1)
 
-print("Counting:")
+print("Counting".center(20, "-"))
 count = browser._retry_delete_manual(old_temp_file)
 print(f"Found {count[0]} files, {count[1]} directories")
 print(f"Errors: {count[2]}")
