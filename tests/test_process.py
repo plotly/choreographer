b353ab3370a5ae39a3d69e60aca88ef1cc81b0e4
Andrew Pikul <ajpikul@gmail.com>
2024-10-16 22:29:42 -0400

    Remove temp file counters from tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -16,7 +16,6 @@ async def test_context(capteesys, headless, debug, debug_browser):
         debug=debug,
         debug_browser=debug_browser,
     ) as browser, timeout(pytest.default_timeout):
-        old_temp_file = str(browser.temp_dir.name)
         response = await browser.send_command(command="Target.getTargets")
         assert "result" in response and "targetInfos" in response["result"]
         assert (len(response["result"]["targetInfos"]) != 0) != headless
@@ -28,26 +27,6 @@ async def test_context(capteesys, headless, debug, debug_browser):
     assert capteesys.readouterr().out == "\n", "stdout should be silent!"
     # let asyncio do some cleaning up if it wants, may prevent warnings
     await asyncio.sleep(0)
-    import sys # TEMP
-    await asyncio.sleep(1)
-    print("Counting".center(20, "-"), file=sys.stderr)
-    count = browser._retry_delete_manual(old_temp_file)
-    print(f"Found {count[0]} files, {count[1]} directories", file=sys.stderr)
-    print(f"Errors: {count[2]}", file=sys.stderr)
-
-
-    await asyncio.sleep(1)
-
-    print("Deleting".center(20, "-"), file=sys.stderr)
-    count = browser._retry_delete_manual(old_temp_file, delete=True)
-    print(f"Errors: {count[2]}", file=sys.stderr)
-
-    await asyncio.sleep(1)
-
-    print("Counting".center(20, "-"), file=sys.stderr)
-    count = browser._retry_delete_manual(old_temp_file)
-    print(f"Found {count[0]} files, {count[1]} directories", file=sys.stderr)
-    print(f"Errors: {count[2]}", file=sys.stderr)
 
 @pytest.mark.asyncio(loop_scope="function")
 async def test_no_context(capteesys, headless, debug, debug_browser):
@@ -56,7 +35,6 @@ async def test_no_context(capteesys, headless, debug, debug_browser):
         debug=debug,
         debug_browser=debug_browser,
     )
-    old_temp_file = str(browser.temp_dir.name)
     try:
         async with timeout(pytest.default_timeout):
             response = await browser.send_command(command="Target.getTargets")
@@ -70,26 +48,6 @@ async def test_no_context(capteesys, headless, debug, debug_browser):
         print("") # this make sure that capturing is working
         assert capteesys.readouterr().out == "\n", "stdout should be silent!"
         await asyncio.sleep(0)
-        import sys # TEMP
-        await asyncio.sleep(1)
-        print("Counting".center(20, "-"), file=sys.stderr)
-        count = browser._retry_delete_manual(old_temp_file)
-        print(f"Found {count[0]} files, {count[1]} directories", file=sys.stderr)
-        print(f"Errors: {count[2]}", file=sys.stderr)
-
-
-        await asyncio.sleep(1)
-
-        print("Deleting".center(20, "-"), file=sys.stderr)
-        count = browser._retry_delete_manual(old_temp_file, delete=True)
-        print(f"Errors: {count[2]}", file=sys.stderr)
-
-        await asyncio.sleep(1)
-
-        print("Counting".center(20, "-"), file=sys.stderr)
-        count = browser._retry_delete_manual(old_temp_file)
-        print(f"Found {count[0]} files, {count[1]} directories", file=sys.stderr)
-        print(f"Errors: {count[2]}", file=sys.stderr)
 
 # we're basically just harrassing choreographer with a kill in this test to see if it behaves well
 @pytest.mark.asyncio(loop_scope="function")
