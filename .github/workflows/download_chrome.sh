378ba149ce92422403629b2d30d7a2823256a255
Andrew Pikul <ajpikul@gmail.com>
2024-11-25 12:10:40 -0500

    Add shebang for bash to script

--- .github/workflows/download_chrome.sh
+++ .github/workflows/download_chrome.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -e
 wget $(curl -s https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json |
 jq -r '.versions.[-1].downloads.chrome.[] | select(.platform == "'"${1}"'").url')
