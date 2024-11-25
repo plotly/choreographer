121ddacc0fbe63d6dbff687128dcb46e43c70f6e
Andrew Pikul <ajpikul@gmail.com>
2024-11-25 12:21:39 -0500

    Try formal jq syntax

--- .github/workflows/download_chrome.sh
+++ .github/workflows/download_chrome.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 wget $(curl -s https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json |
-jq -r ".versions.[-1].downloads.chrome.[] | select(.platform == \"${1}\").url")
+jq -r ".versions | .[-1] | .downloads.chrome | .[] | select(.platform == \"${1}\").url")
 unzip chrome*
 export BROWSER_PATH="$(realpath chrome-linux64)/chrome"
