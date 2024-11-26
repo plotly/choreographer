2e801a139cc351f96c1d1d7b8082cedd5e3c9554
Andrew Pikul <ajpikul@gmail.com>
2024-11-25 21:26:10 -0500

    Try bash script on windows GHA

--- .github/workflows/download_chrome.sh
+++ .github/workflows/download_chrome.sh
@@ -3,4 +3,4 @@ set -e
 wget $(curl -s https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json |
 jq -r ".versions | .[-1] | .downloads.chrome | .[] | select(.platform == \"${1}\").url")
 unzip chrome*
-export BROWSER_PATH="$(realpath chrome-linux64)/chrome"
+export BROWSER_PATH="$(realpath "chrome-${1}")/chrome"
