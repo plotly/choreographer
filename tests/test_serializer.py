40719d94a260b20bf03c25263642a8e06aea1bfa
Andrew Pikul <ajpikul@gmail.com>
2024-12-09 13:49:48 -0500

    Add test for serializing and deserializing data

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -7,4 +7,9 @@ data = [1, 2.00, 3, float("nan"), float("inf"), float("-inf"), datetime.today()]
 def test_de_serialize():
     pipe = Pipe()
     obj = pipe.serialize(data)
+    print(obj)
     pipe.deserialize(obj[:-1]) # split out \0
+
+
+# TODO: Not sure if this all the data we have to worry about:
+# we should also run through mocks and have it print data.
