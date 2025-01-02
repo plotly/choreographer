ba362874b7ea4889494b1c30fe061faa9c54ed75
Andrew Pikul <ajpikul@gmail.com>
2025-01-02 17:52:23 -0500

    Change importation strategy

--- choreographer/__init__.py
+++ choreographer/__init__.py
@@ -2,10 +2,16 @@
 
 from ._browser import Browser, browser_which, get_browser_path
 from ._cli_utils import get_browser, get_browser_sync
+from ._devtools_protocol_layer._session import Session
+from ._devtools_protocol_layer._target import Target
 from ._system_utils._tempfile import TempDirectory, TempDirWarning
+from ._tab import Tab
 
 __all__ = [
     "Browser",
+    "Session",
+    "Tab",
+    "Target",
     "TempDirWarning",  # just for testing?
     "TempDirectory",  # just for testing?
     "browser_which",
