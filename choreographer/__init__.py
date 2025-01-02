0c7864f17b5bf34420985f4f82c691d3fde5e493
Andrew Pikul <ajpikul@gmail.com>
2025-01-01 21:39:20 -0500

    Reorganize directories

--- choreographer/__init__.py
+++ choreographer/__init__.py
@@ -1,13 +1,15 @@
+"""choreographer is a browser controller for python."""
+
 from .browser import Browser, browser_which, get_browser_path
 from .cli_utils import get_browser, get_browser_sync
 from .tempfile import TempDirectory, TempDirWarning
 
 __all__ = [
-    Browser,
-    get_browser,
-    get_browser_sync,
-    browser_which,
-    get_browser_path,
-    TempDirectory,
-    TempDirWarning,
+    "Browser",
+    "TempDirWarning",
+    "TempDirectory",
+    "browser_which",
+    "get_browser",
+    "get_browser_path",
+    "get_browser_sync",
 ]
