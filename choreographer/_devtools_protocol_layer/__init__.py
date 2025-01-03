0a13e386643b0e3661f1af3ffd6c256214b114ff
Andrew Pikul <ajpikul@gmail.com>
2025-01-03 12:38:24 -0500

    Export more errors

--- choreographer/_devtools_protocol_layer/__init__.py
+++ choreographer/_devtools_protocol_layer/__init__.py
@@ -1,10 +1,17 @@
-from ._protocol import DevtoolsProtocolError, MessageTypeError
+from ._protocol import (
+    DevtoolsProtocolError,
+    ExperimentalFeatureWarning,
+    MessageTypeError,
+    MissingKeyError,
+)
 from ._session import Session
 from ._target import Target
 
 __all__ = [
     "DevtoolsProtocolError",
+    "ExperimentalFeatureWarning",
     "MessageTypeError",
+    "MissingKeyError",
     "Session",
     "Target",
 ]
