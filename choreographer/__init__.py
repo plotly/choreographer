479c4090553687b461397ee524be0af28d207772
Andrew Pikul <ajpikul@gmail.com>
2025-01-06 12:08:17 -0500

    Fix imports

--- choreographer/__init__.py
+++ choreographer/__init__.py
@@ -8,18 +8,17 @@ as a building block.
 
 from choreographer import protocol
 
-from ._browser import Browser, BrowserClosedError
-from ._channels.pipe import BlockWarning, PipeClosedError
+from ._browser_sync import BrowserClosedError, BrowserSync, TabSync
+from ._channels import BlockWarning, ChannelClosedError
 from ._cli_utils import get_browser, get_browser_sync
 from ._sys_utils import TempDirectory, TempDirWarning, browser_which, get_browser_path
-from ._tab import Tab
 
 __all__ = [
     "BlockWarning",
-    "Browser",
     "BrowserClosedError",
-    "PipeClosedError",
-    "Tab",
+    "BrowserSync",
+    "ChannelClosedError",
+    "TabSync",
     "TempDirWarning",
     "TempDirectory",
     "browser_which",
