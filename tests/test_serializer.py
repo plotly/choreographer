7a73a5998a97dd50372bfd8696baaf4bb4dd7051
Andrew Pikul <ajpikul@gmail.com>
2025-01-08 10:28:36 -0500

    Lint test_serializer

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,10 +1,10 @@
-from datetime import datetime, timezone
+from datetime import UTC, datetime
 
 import numpy as np
 
 from choreographer._pipe import Pipe
 
-_timestamp = datetime(1970, 1, 1, tzinfo=timezone.utc)
+_timestamp = datetime(1970, 1, 1, tzinfo=UTC)
 
 data = [1, 2.00, 3, float("nan"), float("inf"), float("-inf"), _timestamp]
 expected_message = b'[1, 2.0, 3, null, null, null, "1970-01-01T00:00:00+00:00"]\x00'
@@ -16,10 +16,10 @@ def test_de_serialize():
     message = pipe.serialize(data)
     assert message == expected_message
     obj = pipe.deserialize(message[:-1])  # split out \0
-    for o, t in zip(obj, converted_type):
+    for o, t in zip(obj, converted_type, strict=False):
         assert isinstance(o, t)
     message_np = pipe.serialize(np.array(data))
     assert message_np == expected_message
     obj_np = pipe.deserialize(message_np[:-1])  # split out \0
-    for o, t in zip(obj_np, converted_type):
+    for o, t in zip(obj_np, converted_type, strict=False):
         assert isinstance(o, t)
