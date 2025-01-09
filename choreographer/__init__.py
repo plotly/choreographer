d324401a3c949146a844cffd8cf883553237db44
Andrew Pikul <ajpikul@gmail.com>
2025-01-09 15:53:13 -0500

    Format for docs

--- choreographer/__init__.py
+++ choreographer/__init__.py
@@ -8,20 +8,20 @@ as a building block.
 See the main README for a quickstart.
 """
 
-from ._browsers import (  # noqa: F401 unused import
+from ._browsers import (
     BrowserClosedError,
     BrowserFailedError,
     Chromium,
 )
-from ._channels import BlockWarning, ChannelClosedError  # noqa: F401 unused import
-from ._cli_utils import get_chrome, get_chrome_sync  # noqa: F401 unused import
-from ._sys_utils import (  # noqa: F401 unused import
+from ._channels import BlockWarning, ChannelClosedError
+from ._cli_utils import get_chrome, get_chrome_sync
+from ._sys_utils import (
     TmpDirectory,
     TmpDirWarning,
     browser_which,
     get_browser_path,
 )
-from .browser_sync import (  # noqa: F401 unused import
+from .browser_sync import (
     BrowserSync,
     SessionSync,
     TabSync,
@@ -29,19 +29,19 @@ from .browser_sync import (  # noqa: F401 unused import
 )
 
 __all__ = [
+    "BlockWarning",
+    "BrowserClosedError",
+    "BrowserFailedError",
     "BrowserSync",
+    "ChannelClosedError",
+    "Chromium",
     "SessionSync",
     "TabSync",
     "TargetSync",
-    "Chromium",
-    "BrowserClosedError",
-    "BrowserFailedError",
-    "ChannelClosedError",
-    "BlockWarning",
     "TmpDirWarning",
-    "get_chrome",
-    "get_chrome_sync",
     "TmpDirectory",
     "browser_which",
     "get_browser_path",
+    "get_chrome",
+    "get_chrome_sync",
 ]
