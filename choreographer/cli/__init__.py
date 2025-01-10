d192d42f3a8694d839d89f449ac7cb4a405acb5a
Andrew Pikul <ajpikul@gmail.com>
2025-01-10 13:56:00 -0500

    Remove unnecessary tools from CLI

--- choreographer/cli/__init__.py
+++ choreographer/cli/__init__.py
@@ -1,17 +1,11 @@
 """cli provides some tools that are used on the commandline (and to download chrome)."""
 
-from ._cli_utils_no_qa import diagnose
-from .cli import (
+from ._cli_utils import (
     get_chrome,
-    get_chrome_cli,
-    get_chrome_download_path,
     get_chrome_sync,
 )
 
 __all__ = [
-    "diagnose",
     "get_chrome",
-    "get_chrome_cli",
-    "get_chrome_download_path",
     "get_chrome_sync",
 ]
