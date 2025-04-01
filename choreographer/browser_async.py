865c6d989c64165a6acd12c4119e7f8ffd3ed952
Emily Kellison-Linn <4672118+emilykl@users.noreply.github.com>
2025-04-01 12:52:03 -0400

    fix typo

--- choreographer/browser_async.py
+++ choreographer/browser_async.py
@@ -152,7 +152,7 @@ class Browser(Target):
                 "The browser seemed to close immediately after starting.",
                 "You can set the `logging.Logger` level lower to see more output.",
                 "You may try installed a known working copy of chrome from ",
-                "`$ choreo_get_chome`. It may be your copy auto-updated.",
+                "`$ choreo_get_chrome`. It may be your copy auto-updated.",
             ) from e
 
     async def __aenter__(self) -> Self:
